c7a553da21a00bd18319787c477922b3dcfe902b
Erick Valencia <evalencia0911@gmail.com>
2023-11-16 12:43:33 -0700

    completed setAlarm

--- solution.js
+++ solution.js
@@ -1,1 +1,18 @@
-// --
+// -- JavaScript Problems Potourri
+
+// --setAlarm
+
+//Write a function named setAlarm
+function setAlarm(employed, vacationing) { //2 arguments employed and vacationing
+    if(employed && !vacationing){; //employed && !vacationing return true. we need to set an alarm
+        return true;
+    }else { //else false, we do not need to set an alarm
+        return false;
+    }
+}
+
+
+console.log(setAlarm(true, true)) // will return false. both employed and vacationing are true
+console.log(setAlarm(false, false)) // will return false. not employed so no alarm is needed
+console.log(setAlarm(false, true)) // will return false. not employed so no alarm is needed AND vactioning
+console.log(setAlarm(true, false)) // will return true. employed and not vacationing

5e3a65ec0a4ab251b7da0d1e4f86dbb056631313
Erick Valencia <evalencia0911@gmail.com>
2023-11-17 14:49:50 -0700

    began work on part 2 of madlib

--- mad_libs/app.js
+++ mad_libs/app.js
@@ -2,6 +2,8 @@ console.log("hello world")
 
 // --MAD LIBS!
 
+
+
 //Part 1 - Building an Object
 
 //First, create an object called words with the keys given below. 
@@ -15,3 +17,16 @@ const words = {
     adverb: ,
     anotherAdjective: ,
 }
+
+// ----------
+
+
+//Part 2 -- Interact With the User
+
+//Now that we have our object set up, we want to work on accepting user input so that we can save it to the object.
+function startMadLib() {
+    prompt(""
+
+
+//Start by creating a function called startMadlib that prompts a user to enter each word type and save their input to the appropriate object key. 
+//For example, the following prompt's user input should be saved to the number key in our words object:

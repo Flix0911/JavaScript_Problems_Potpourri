bf9aeccedfd75b3e101d1adb8e08447d54691d73
Erick Valencia <evalencia0911@gmail.com>
2023-11-17 14:32:58 -0700

    added obj called words

--- mad_libs/app.js
+++ mad_libs/app.js
@@ -1,1 +1,17 @@
 console.log("hello world")
+
+// --MAD LIBS!
+
+//Part 1 - Building an Object
+
+//First, create an object called words with the keys given below. 
+//Each key's property should be an empty string. 
+//This is where we will save the user's input later.
+
+const words = {
+    number: ,
+    adjective: ,
+    pluralNoun: ,
+    adverb: ,
+    anotherAdjective: ,
+}

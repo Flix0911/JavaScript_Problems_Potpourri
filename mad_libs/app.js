78cdc3ffabdcdc9305cb61fbdd546831e8eeb934
Erick Valencia <evalencia0911@gmail.com>
2023-11-19 10:31:12 -0700

    working on function StartMadLib

--- mad_libs/app.js
+++ mad_libs/app.js
@@ -11,11 +11,11 @@ console.log("hello world")
 //This is where we will save the user's input later.
 
 const words = {
-    number: ,
-    adjective: ,
-    pluralNoun: ,
-    adverb: ,
-    anotherAdjective: ,
+    number: '',
+    adjective: '',
+    pluralNoun: '',
+    adverb: '',
+    anotherAdjective: '',
 }
 
 // ----------
@@ -23,10 +23,20 @@ const words = {
 
 //Part 2 -- Interact With the User
 
-//Now that we have our object set up, we want to work on accepting user input so that we can save it to the object.
+//Now that we have our object set up, we want to work on accepting user input 
+//so that we can save it to the object.
 function startMadLib() {
-    prompt(""
+   let number = prompt("Enter a number!"); {
+    //number = wordsNumber(number)
+   console.log(number); //~ confirming it works
+   } let adjective = prompt ("Enter an adjective"); {
+    //adjective = wordsAdjective(adjective)
+    console.log(adjective)
+   }
 
+}
+
+startMadLib()
 
 //Start by creating a function called startMadlib that prompts a user to enter each word type and save their input to the appropriate object key. 
 //For example, the following prompt's user input should be saved to the number key in our words object:

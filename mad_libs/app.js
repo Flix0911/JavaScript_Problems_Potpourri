114272871600845e72ddff03bbd2c0917e278d05
Erick Valencia <evalencia0911@gmail.com>
2023-11-20 13:53:14 -0700

    changed some comments

--- mad_libs/app.js
+++ mad_libs/app.js
@@ -29,31 +29,28 @@ function startMadlib() { //my function called startMadlib
     {
         alert("This is a madlibs game. You will be prompted to submit certain responses. At the end, your responses will be plugged into the whole paragraph. Lets begin!");
 
-    }
-    let chosenNumber = prompt("Enter a number!"); {
-    words.number = chosenNumber; //I believe this is correct but I don't understand why
-    //chosenNumber = words.number(number) //This is incorrect and I know that now. Why though?
-    console.log(words.number = chosenNumber); //confirming number works
-   } 
-   
-    let chosenAdjective = prompt("Enter an adjective"); {
-    words.adjective = chosenAdjective;//I believe this is correct but I don't understand why
+    }let chosenNumber = prompt("Enter a number!"); {
+         words.number = chosenNumber;
+    //chosenNumber = words.number(number) //I now understand why, thank you Kyle!
+            console.log(words.number = chosenNumber); //confirming number works
+   } let chosenAdjective = prompt("Enter an adjective"); {
+    words.adjective = chosenAdjective;
     //chosenAdjective = words.adjective(adjective)
     console.log(chosenAdjective) //confirming adjective works
    } 
    
     let chosenPluralNoun = prompt("Enter a plural noun"); {
-    words.pluralNoun = chosenPluralNoun;//I believe this is correct but I don't understand why
+    words.pluralNoun = chosenPluralNoun;/
     console.log(chosenPluralNoun) //confirming pluralNoun works
    } 
    
     let chosenAdverb = prompt("Enter an adverb"); {
-    words.adverb = chosenAdverb;//I believe this is correct but I don't understand why
+    words.adverb = chosenAdverb;
     console.log(chosenAdverb)//cofirming adverb works
    } 
    
     let chosenAnotherAdjective = prompt("Enter another adjective"); {
-    words.anotherAdjective = chosenAnotherAdjective//I believe this is correct but I don't understand why
+    words.anotherAdjective = chosenAnotherAdjective/
     console.log(chosenAnotherAdjective)//confirming anotherAdjective works
    }
 
@@ -75,6 +72,10 @@ startMadlib() //will invoke the function to begin upon opening the page
 //reset function
 
 //create a function named restartMadlib
+
+function restartMadLib() {
+
+}
 //reassign our words object with empty string values / aka: empty our words object
 
 
